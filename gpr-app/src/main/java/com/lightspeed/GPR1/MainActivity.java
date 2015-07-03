8d764b59f7d1e341920f8067ce627a8a767d872f
Jack Hosemans <jh247247@gmail.com>
2015-07-03 23:18:18 +1000

    fixed a dependencies on test file

--- gpr-app/src/main/java/com/lightspeed/GPR1/MainActivity.java
+++ gpr-app/src/main/java/com/lightspeed/GPR1/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
 import android.content.Context;
-import a.Simple;
 
 public class MainActivity extends Activity
 {
