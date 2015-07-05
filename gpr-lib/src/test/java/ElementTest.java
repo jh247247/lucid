7c151415ee224de51670dd583913f0391a8126e9
Jack Hosemans <jh247247@gmail.com>
2015-07-05 22:47:18 +1000

    Implemented element clone test

--- gpr-lib/src/test/java/ElementTest.java
+++ gpr-lib/src/test/java/ElementTest.java
@@ -14,9 +14,9 @@ public class ElementTest {
 
     @Test
     public void test_set() {
-	Element e = new Element(1);
-	e.setSample(0,1);
-	Assert.assertEquals(e.getSample(0),1);
+        Element e = new Element(1);
+        e.setSample(0,1);
+        Assert.assertEquals(e.getSample(0),1);
     }
 
     @Test
@@ -28,6 +28,30 @@ public class ElementTest {
         int curr = 0;
         for (int i : e) {
             Assert.assertEquals(curr++, i);
+        }
+    }
+
+    @Test
+    public void test_clone() {
+        Element e = new Element(10);
+        for(int i = 0; i < e.getAmountOfSamples(); i++) {
+            e.setSample(i,i);
+        }
+
+        Element r = e.clone();
+
+        // test equality
+        Assert.assertEquals(r.getAmountOfSamples(),
+                            e.getAmountOfSamples());
+
+        for(int i = 0; i < r.getAmountOfSamples(); i++) {
+            Assert.assertEquals(r.getSample(i), e.getSample(i));
+            r.setSample(i, r.getSample(i)+1);
+        }
+
+        // test change
+        for(int i = 0; i < r.getAmountOfSamples(); i++) {
+            Assert.assertEquals(r.getSample(i), e.getSample(i)+1);
         }
     }
 }
