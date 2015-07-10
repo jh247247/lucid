45f7769167d3b09ec19400ae1d8c1e9cf68b42e3
Jack Hosemans <jh247247@gmail.com>
2015-07-10 17:53:01 +1000

    Changed running average seeding process, started writing tests for average

---
+++ gpr-lib/src/test/java/AverageTest.java
@@ -0,0 +1,15 @@
+package DataHandler;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.Iterator;
+import com.lightspeed.gpr.lib.Element;
+import com.lightspeed.gpr.lib.Average;
+
+
+public class AverageTest {
+    @Test
+    public void test_instantiation() {
+	Average a = new Average();
+    }
+}
