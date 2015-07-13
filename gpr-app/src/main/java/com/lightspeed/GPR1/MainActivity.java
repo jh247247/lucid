5107572f2454983c117e49894ea206f573366e20
Jack Hosemans <jh247247@gmail.com>
2015-07-13 22:37:23 +1000

    Changed android app to add rendering to bitmap for later use

--- gpr-app/src/main/java/com/lightspeed/GPR1/MainActivity.java
+++ gpr-app/src/main/java/com/lightspeed/GPR1/MainActivity.java
@@ -1,17 +1,26 @@
 package com.lightspeed.GPR1;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.widget.Toast;
-import android.content.Context;
+import android.view.Menu;
+import android.widget.LinearLayout;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class MainActivity extends Activity {
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.main);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Paint paint = new Paint();
+        paint.setColor(Color.parseColor("#CD5C5C"));
+        Bitmap bg = Bitmap.createBitmap(480, 800, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bg);
+        canvas.drawRect(50, 50, 200, 200, paint);
+        LinearLayout ll = (LinearLayout) findViewById(R.id.rect);
+        ll.setBackgroundDrawable(new BitmapDrawable(bg));
     }
 }
