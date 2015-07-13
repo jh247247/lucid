6cd347fff452890d6b6452f5e6a1f243a721fa87
Jack Hosemans <jh247247@gmail.com>
2015-07-13 19:13:14 +1000

    Added interface for data input.

---
+++ gpr-lib/src/main/java/com/lightspeed/lib/DataInputInterface.java
@@ -0,0 +1,29 @@
+package com.lightspeed.gpr.lib;
+import com.lightspeed.gpr.lib.Element;
+
+public interface DataInputInterface {
+    /**
+     * Should return null if there is no data to grab
+     * Otherwise should pop off the next element of data
+     */
+    public Element getNext();
+
+    /**
+     * Should return true if there is data in the queue
+     * Otherwise return false.
+     */
+    public boolean hasNext();
+
+    /**
+     * Attempts to open an interface, hide all the inner workings so
+     * it can be a file, serial port or whatevs. Should be able to put
+     * up it's own interface to work. ASSUME THIS BLOCKS
+     */
+    public boolean open();
+
+    /**
+     * Closes the opened interface. Should there be anything special
+     * about this?
+     */
+    public void close();
+}
