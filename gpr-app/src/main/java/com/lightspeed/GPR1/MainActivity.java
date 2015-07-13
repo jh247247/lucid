884f276ca8f01630cb20988ce94f56c23e9b5c20
Jack Hosemans <jh247247@gmail.com>
2015-07-14 00:31:11 +1000

    Finish commit, should stage everything next time

--- gpr-app/src/main/java/com/lightspeed/GPR1/MainActivity.java
+++ gpr-app/src/main/java/com/lightspeed/GPR1/MainActivity.java
@@ -9,18 +9,55 @@ import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.LinearLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends ActionBarActivity {
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        Paint paint = new Paint();
-        paint.setColor(Color.parseColor("#CD5C5C"));
-        Bitmap bg = Bitmap.createBitmap(480, 800, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bg);
-        canvas.drawRect(50, 50, 200, 200, paint);
-        LinearLayout ll = (LinearLayout) findViewById(R.id.rect);
-        ll.setBackgroundDrawable(new BitmapDrawable(bg));
+        setContentView(R.layout.activity_my);
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+										this,
+										drawerLayout,
+										toolbar,
+										R.string.open,
+										R.string.close
+										)
+
+	    {
+		public void onDrawerClosed(View view)
+		{
+		    super.onDrawerClosed(view);
+		    invalidateOptionsMenu();
+		    syncState();
+		}
+
+		public void onDrawerOpened(View drawerView)
+		{
+		    super.onDrawerOpened(drawerView);
+		    invalidateOptionsMenu();
+		    syncState();
+		}
+	    };
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //Set the custom toolbar
+        if (toolbar != null){
+	    toolbar.getBackground().setAlpha(50);
+            setSupportActionBar(toolbar);
+        }
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        actionBarDrawerToggle.syncState();
     }
 }
